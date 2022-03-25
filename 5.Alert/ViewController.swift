fbd0278c2a809b524b9128837976f86c114785eb
ByeongHyeon <qudgus1984@naver.com>
2022-03-26 01:17:47 +0900

    얼럿 완료

--- 5.Alert/ViewController.swift
+++ 5.Alert/ViewController.swift
@@ -37,9 +37,44 @@ class ViewController: UIViewController {
     }
     
     @IBAction func btnLampOff(_ sender: UIButton) {
+        if isLampOn {
+            let lampOffAlert = UIAlertController(title: "램프 끄기", message: "램프를 끄시겠습니까?", preferredStyle: UIAlertController.Style.alert) // UIAlertController 생성
+            
+            let offAction = UIAlertAction(title: "네",
+                style: UIAlertAction.Style.default, handler:  {
+                ACTION in self.lampImg.image = self.imgOff
+                self.isLampOn=false // UIAlertAction 생성.
+            })
+            let cancelAction = UIAlertAction(title: "아니요", style: UIAlertAction.Style.default, handler: nil)
+            // UIAlertAction를 추가로 생성 특별한 동작 하지 않을 경우 핸들러를 nil로 함
+            
+            lampOffAlert.addAction(offAction)
+            lampOffAlert.addAction(cancelAction) // 생성된 offAction, cancelAction을 얼럿에 추가
+            
+            present(lampOffAlert, animated: true, completion: nil) // present 메서드를 실행
+        }
     }
     
     @IBAction func btnLampRemove(_ sender: UIButton) {
+        let lampRemoveAlert = UIAlertController(title: "램프 제거", message: "램프를 제거하시겠습니까?", preferredStyle: UIAlertController.Style.alert) // UIAlertController 생성
+        
+        let offAction = UIAlertAction(title: "아니오, 끕니다(off).", style: UIAlertAction.Style.default, handler: {
+            ACTION in self.lampImg.image = self.imgOff
+            self.isLampOn=false
+        }) // UIAlertAction을 생성. 전구를 꺼야하므로 handler에 {,}을 넣어 추가적으로 작업
+        let onAction = UIAlertAction(title: "아니오, 켭니다(on).", style: UIAlertAction.Style.default) {
+            ACTION in self.lampImg.image = self.imgOn
+            self.isLampOn=true
+        } // UIAlertAction을 생성. 전구를 켜야하므로 handler에 {,}을 넣어 추가적으로 작업
+        let removeAction = UIAlertAction(title: "네, 제거합니다.", style: UIAlertAction.Style.destructive, handler: {
+            ACTION in self.lampImg.image = self.imgRemove
+            self.isLampOn=false
+        }) // UIAlertAction을 추가로 생성한 후 전구를 제거하는 동작을 추가
+        
+        lampRemoveAlert.addAction(offAction)
+        lampRemoveAlert.addAction(onAction)
+        lampRemoveAlert.addAction(removeAction) // 생성된 on,off,removeAction을 얼럿에 추가
+        present(lampRemoveAlert, animated: true, completion: nil) // present 메서드를 실행
     }
 }
 
